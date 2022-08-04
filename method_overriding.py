72fd21c1dd8db56b38c0051e4c2513f60f5aece7
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-08-04 16:53:00 +0530

    Add files via upload

---
+++ method_overriding.py
@@ -0,0 +1,9 @@
+# class parent():
+#     def study (self):
+#         print("keep studying")
+
+# class child(parent):
+#     def study(self):
+#         print("take a break")
+# c=child()
+# c.study()             take a break
